19b7935d14f83e687e241fe98e7907de20ffded6
Luca Lianas <luca.lianas@crs4.it>
2016-08-26 17:14:28 +0200

    first working version of decorator and methods to retrieve DZI and tiles

---
+++ ome_seadragon_gateway/urls.py
@@ -0,0 +1,36 @@
+"""ome_seadragon_gateway URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from viewer_gw.views import OmeDZIWrapper, OmeTileWrapper
+
+urlpatterns = [
+    # DZI files
+    url(r'api/dzi/(?P<image_id>[0-9]+)/$', OmeDZIWrapper.as_view()),
+
+    # TILES
+    url(r'api/tiles/(?P<image_id>[0-9]+)_files/(?P<level>[0-9]+)/'
+        r'(?P<column>[0-9]+)_(?P<row>[0-9]+).(?P<image_format>[\w]+)$',
+        OmeTileWrapper.as_view()),
+
+    # admin
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ viewer_gw/views.py
@@ -0,0 +1,56 @@
+from urlparse import urljoin
+
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotAuthenticated
+
+from django.http import HttpResponse
+
+import ome_seadragon_gateway.settings as gws
+from ome_seadragon_gateway.decorators import ome_session_required
+
+import logging
+logger = logging.getLogger('ome_seadragon_gw')
+
+
+class OmeDZIWrapper(APIView):
+
+    @ome_session_required
+    def get(self, request, client, image_id, format=None):
+        uri = urljoin(
+            gws.OME_SEADRAGON_BASE_URL,
+            'deepzoom/get/%s.dzi' % image_id
+        )
+        logger.info('Paylod: %s', uri)
+        response = client.get(uri, headers={'X-Requested-With': 'XMLHttpRequest'})
+        logger.info('STATUS CODE: %d', response.status_code)
+        if response.status_code == status.HTTP_200_OK:
+            return HttpResponse(
+                response.content, status=status.HTTP_200_OK,
+                content_type=response.headers.get('content-type')
+            )
+        else:
+            logger.error('ERROR')
+            raise NotAuthenticated()
+
+
+class OmeTileWrapper(APIView):
+
+    @ome_session_required
+    def get(self, request, client, image_id, level, column, row, image_format, format=None):
+        uri = urljoin(
+            gws.OME_SEADRAGON_BASE_URL,
+            'deepzoom/get/%s_files/%s/%s_%s.%s' % (image_id, level, column, row, image_format)
+        )
+        logger.info('Payload: %s', uri)
+        response = client.get(uri, headers={'X-Requested-With': 'XMLHttpRequest'})
+        logger.info('STATUS CODE: %d', response.status_code)
+        if response.status_code == status.HTTP_200_OK:
+            logger.info(response.headers.get('content-type'))
+            return HttpResponse(
+                response.content, status=status.HTTP_200_OK,
+                content_type=response.headers.get('content-type'))
+        else:
+            logger.error('ERROR')
